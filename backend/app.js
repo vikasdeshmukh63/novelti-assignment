15df72bac0111c676278cd1c2c0304fa904c8a86
Vikas Deshmukh <114295008+vikasdeshmukh63@users.noreply.github.com>
2023-07-02 16:37:30 +0530

    update for deploy

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,11 @@ const userRoutes = require("./routes/userRoute");
 const phoneRoutes = require("./controllers/phoneCodeController")
 
 // using middleware
-app.use(cors());
+app.use(cors({
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+}));
 app.use(express.json());
 
 // connecting to database
